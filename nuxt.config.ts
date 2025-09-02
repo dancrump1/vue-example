eb9a50c88036fd1ace7ccb3da6c724d79e91d636
dancrump1 <danielcrump@outlook.com>
2025-09-02 16:27:17 -0400

    fix build and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
-    plugins: [
-        vue(),
-        vueJsx(),
-        vueDevTools(),
-    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
